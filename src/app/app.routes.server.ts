a11bb04f73c4141ec6d1c4834e9f40835d3c3507
Marvin Aroza <marvin.aroza@silversmilesolutions.com>
2025-07-26 13:25:33 +0100

    feat(AAP1): resolve console errors

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '/login',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: '**',
-    renderMode: RenderMode.SSR
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
